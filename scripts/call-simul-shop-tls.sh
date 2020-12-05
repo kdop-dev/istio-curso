65ca132ab3358edda72106725bd4a88a7fa943ec
Anderson Santos <adsantos@gmail.com>
2020-12-05 19:40:59 -0300

    fix gateway, add security - in progress

--- scripts/call-simul-shop-tls.sh
+++ scripts/call-simul-shop-tls.sh
@@ -7,7 +7,17 @@ while true
 do
 curl -v -H "Host: www.simul-shop.com" \
     --resolve "www.simul-shop.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
-    --cacert exemplos/simul-shop/certs/www.simul-shop.com.crt "https://www.simul-shop.com:$SECURE_INGRESS_PORT/s"
+    --cacert exemplos/simul-shop/certs/all.simul-shop.com.crt "https://www.simul-shop.com:$SECURE_INGRESS_PORT/s"
 echo
 sleep 1
 done
+
+# TODO: Configurar certificado para reconhecer simul-shop.com
+# while true
+# do
+# curl -v -H "Host: simul-shop.com" \
+#     --resolve "simul-shop.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+#     --cacert exemplos/simul-shop/certs/all.simul-shop.com.crt "https://simul-shop.com:$SECURE_INGRESS_PORT/s"
+# echo
+# sleep 1
+# done
--- scripts/call-simul-shop.sh
+++ scripts/call-simul-shop.sh
@@ -2,8 +2,11 @@
 
 while true
 do
-#curl --header "Host: simul-shop.com/s
-curl http://simul-shop.com/s
+## Sem modificar o /etc/hosts
+#curl -v --header "Host: simul-shop.com" http://localhost/s
+#curl -v -H "Host: www.simul-shop.com" --resolve "www.simul-shop.com:80:127.0.0.2" http://www.simul-shop.com/s
+## Resolvendo pelo /etc/hosts
+curl -v http://simul-shop.com/s
 echo
 sleep 1
 done
